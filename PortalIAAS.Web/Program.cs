7d0ba80447f5b88cffd11c2ece7637eff6d2d3c7
eleaz <eleaz@DESKTOP-834FADU>
2026-03-10 09:00:10 -0600

    Se agregó MudBlazor a PortalIAAS.Web.

--- PortalIAAS.Web/Program.cs
+++ PortalIAAS.Web/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
+using MudBlazor.Services;
 using PortalIAAS.Web.Components;
+using PortalIAAS.Web.UI.Notificaciones;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +18,22 @@ builder.Services.AddAuthorization(options =>
 });
 
 builder.Services.AddCascadingAuthenticationState();
+
+// Agregar MudBlazor
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass =
+        MudBlazor.Defaults.Classes.Position.TopRight;
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = true;
+    config.SnackbarConfiguration.MaxDisplayedSnackbars = 3;
+    config.SnackbarConfiguration.VisibleStateDuration = 5000;
+    config.SnackbarConfiguration.ShowTransitionDuration = 100;
+    config.SnackbarConfiguration.HideTransitionDuration = 100;
+    config.SnackbarConfiguration.RequireInteraction = false;
+});
+
+builder.Services.AddScoped<NotificacionService>();
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents(options =>
@@ -49,5 +68,17 @@ app.UseAntiforgery();
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapGet("/logout", async (HttpContext context) =>
+{
+    await context.SignOutAsync(); // limpia cookie local
+
+    await context.SignOutAsync(
+        OpenIdConnectDefaults.AuthenticationScheme,
+        new AuthenticationProperties
+        {
+            RedirectUri = "/"
+        });
+});
 
 app.Run();
---
+++ PortalIAAS.Web/UI/Notificaciones/TipoNotificacion.cs
@@ -0,0 +1,10 @@
+﻿namespace PortalIAAS.Web.UI.Notificaciones
+{
+    public enum TipoNotificacion
+    {
+        Informacion = 1,
+        Exito,
+        Advertencia,
+        Error
+    }
+}
